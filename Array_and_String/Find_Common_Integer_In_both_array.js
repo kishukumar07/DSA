6a0c67ff912321108fe7cd29907a7cb4c847062e
kishukumar07 <kishukumars082@gmail.com>
2024-12-31 21:00:44 +0530

    added

---
+++ Array_and_String/Find_Common_Integer_In_both_array.js
@@ -0,0 +1,29 @@
+function findCommonEL(arr1, arr2){
+    //method1.BruteForceApproach =>> O(n**2)
+//taking with two ptr =>>O(n) or taking hasmap =>> O{n}
+
+//if we using two ptr we have to sort the array first if not sorted=>O(n) for bestCase ifarray is already sorted nd O(nlogn) if array is not sorted 
+
+arr1.sort((a, b) => a - b); 
+arr2.sort((a, b) => a - b); 
+let el; 
+let i=0 ,j=0;  
+while(i<arr1.length && j<arr2.length){  //here we using and operator bcs array is sorted 
+    if(arr1[i]===arr2[j]){
+        // console.log(arr1[i],arr2[j]); 
+              el =arr1[i];
+              break;  
+    }
+else if(arr1[i]<arr2[j]){
+    i++; 
+}
+else if(arr1[i]>arr2[j]){
+    j++; 
+}
+}
+
+return el ; 
+}
+
+console.log(findCommonEL([1,2,3,4,5] , [9,6,9,6,,5]));  
+
--- Array_and_String/No_of_Pairs_whose_sum_is_equal_to_k.js
+++ Array_and_String/No_of_Pairs_whose_sum_is_equal_to_k.js
@@ -1,12 +1,15 @@
-function pairs(arr, k) {
+//with brute force approach it will going to be O(n**2)
 
+
+
+function pairs(arr, k) {
 
     let count = 0;
     let l = 0, r = arr.length - 1;
 
     //using two pointers we can solve it in O(n) if array is already sorted  and if array is not shorted we have to sort it that lead to use of arr.sort() method && it will make O(nlogn)+O(n)   : O(nlogn)will becomes O(1) if already sorted
 
-    arr.sort((a, b) => { a - b }); //O(nlogn)    
+   arr.sort((a, b) => a - b); //O(nlogn)    
 
     while (l < r) {  //O(n)
         if (arr[l] + arr[r] === k) {
