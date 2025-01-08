51ee32d6b1956c7c5c87dd076478ccdd1b6cd300
kishukumar07 <kishukumars082@gmail.com>
2025-01-08 14:44:04 +0530

    BubbleSort

---
+++ SortingTechniques/BubbleSort/01.concept.js
@@ -0,0 +1,34 @@
+// you are given a array and you need to short this array either in accending order or decending order 
+
+
+let arr=[3,5,6,1,5,9,2,0]; //O{N-squire}
+// let arr=[0,1,2,3,4];  //O{N}
+
+let n=arr.length; 
+
+console.log(bubbleSort(arr,n)); 
+
+function bubbleSort(arr , n ){
+
+for(let i=0; i<=n-2; i++){
+    let isSwaped=false; 
+for(let j=0;  j<=n-1-i; j++){
+
+    if(arr[j]  > arr[j+1]){
+       let temp =arr[j]; 
+       arr[j]=arr[j+1];
+       arr[j+1]=temp;
+      isSwaped=true; 
+    }
+
+}
+if(isSwaped===false){
+break; 
+}
+
+}
+
+return arr; 
+
+
+}
