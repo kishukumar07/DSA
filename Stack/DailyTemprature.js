e168e6203e3bef3a468f79b1cc92aff40316f5c1
kishukumar07 <kishukumars082@gmail.com>
2025-01-22 11:34:18 +0530

    added

---
+++ Stack/DailyTemprature.js
@@ -0,0 +1,5 @@
+let arr=[73,74,75,71,69,72,76,73];  
+
+
+//goal : => reslut array : a[i] = no of days to get a warm temp . if no such day then a[i]=0
+//Approach we have to find the greater element from right  and how many pops we have done will be our day
