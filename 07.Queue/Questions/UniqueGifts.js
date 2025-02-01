b6a5609557882affb3bffb259918f3fba3860667
Kishukumar07 <kishukumars082@gmail.com>
2025-02-01 17:55:49 +0530

    added

---
+++ 07.Queue/Questions/UniqueGifts.js
@@ -0,0 +1,32 @@
+
+
+// let str="abadbc"; 
+let str="abcabc"; 
+
+function unique(str){
+
+var dict={}; 
+var queue=[]; 
+let i=0; 
+
+while(i<=str.length-1){
+if(dict[str[i]]===undefined){
+    dict[str[i]]=1; 
+    queue.push(str[i]); 
+}else{
+    dict[str[i]]++; 
+queue.shift(); 
+}
+
+if(queue.length!==0){
+    console.log(queue[0]); 
+}else{
+    console.log("#")
+}
+    i++; 
+}
+
+}
+
+
+unique(str); 
