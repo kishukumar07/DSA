9beb71f793c04c2da33d88ef867b4bb1bfc9e4aa
Kishukumar07 <kishukumars082@gmail.com>
2025-02-05 10:39:21 +0530

    added

---
+++ 02.SubArray_SubString/maxoccOddelement.js
@@ -0,0 +1,16 @@
+function longestRepeatedOdd(N, array) {
+    // Write code here
+    let count = 0;
+    let maxc=0; 
+    for (let i = 0; i <= array.length-1; i++){
+        if (array[i] % 2 !== 0){
+            count++
+        }else if (array[i] % 2 === 0){
+            count =0; 
+            continue;
+        }
+     maxc<count? maxc=count:''
+    }
+    console.log(maxc)
+}
+longestRepeatedOdd(10,[2,1,1,2,1,1,1,1,1,0]); 
