bd5c8f20b310cf1cd9549b5d070dc80ba47c6e5f
Kishukumar07 <kishukumars082@gmail.com>
2025-02-19 00:16:48 +0530

    lesson

---
+++ LeetcodePOTDSolutions/07.js
@@ -0,0 +1,7 @@
+// Spent 10 hours on a problem—brute force failed. 
+// // Lesson: Wrong approaches waste time. Learn, adapt, optimize!
+// Lesson learned:
+// ✅ Don't force a wrong approach
+// ✅ Recognizing inefficiency = growth
+// ✅ Backtracking > blind permutations
+// ✅ Failure today = strength tomorrow
