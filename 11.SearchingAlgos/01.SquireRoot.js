7f50f7c036a17cbabc8d3d6f14c055dea5a05429
Kishukumar07 <kishukumars082@gmail.com>
2025-04-13 20:48:18 +0530

    squireroot without Math.sqrt()

---
+++ 11.SearchingAlgos/01.SquireRoot.js
@@ -0,0 +1,19 @@
+//find if a given no is perfect squire of another number  without using squire root mean : without finding squire root of that number
+
+//Math.sqrt(num) is banned 
+
+
+
+/*
+ linear search ->  run a loop till num  
+                     -> -will make check if (i squire === num )  return i ; 
+                     ->    =>O(n)
+
+ bbl search    ->  will mentain a arr =[1,2,3,4.....num-1 ]  -> O(n) space and time 
+                     -> for the array we'll check weather the middleEl in the space 
+                           -> squireofmiddleEl === num  -> we got that ele
+                           ->   "        "     <   num  -> we have to move left part /space 
+                           ->   "        "     >   num  -> we have to move for the right part/space 
+                
+                           -> => O(log-n)
+*/
