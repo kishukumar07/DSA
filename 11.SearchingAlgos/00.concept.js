0bcf58e2fc24d7c048aec21affc4b3613b03a738
Kishukumar07 <kishukumars082@gmail.com>
2025-04-13 18:21:06 +0530

    solved this to one concept

--- 11.SearchingAlgos/00.concept.js
+++ 11.SearchingAlgos/00.concept.js
@@ -21,6 +21,29 @@ console.log(result)
 
 
 
+//when array is sorted --- BINARY SEARCH ALGORITHM ....
+
+let result2 = binarySearch([1, 2, 3, 4, 5, 34, 675, 787], 675)
+
+function binarySearch(arr, key, low = 0, high = arr.length) {
+    if (low > high) {
+        return -1
+    }; //base case when they cross
+
+    let mid = Math.floor(low + ((high - low) / 2));  //main case 
+
+    if (arr[mid] == key) {
+        return mid;
+    }
 
 
+    else if (arr[mid] < key) {
+        return binarySearch(arr, key, mid + 1, high)//moving to left part  
+        //recursive callings on certain conditions ...
+    }
+    else {
+        return binarySearch(arr, key, low, mid - 1);  //moving to right part
+    }
+}
 
+console.log(result2);  // serched index of the key  
