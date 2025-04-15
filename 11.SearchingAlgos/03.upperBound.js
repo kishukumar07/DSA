d8b43b7e251c92074791a5b1f3152526f51e2599
Kishukumar07 <kishukumars082@gmail.com>
2025-04-15 16:27:52 +0530

    uppper bound with binary search

--- 11.SearchingAlgos/03.upperBound.js
+++ 11.SearchingAlgos/03.upperBound.js
@@ -5,7 +5,7 @@ function lowerbound(arr, key, low = 0, high = arr.length - 1, result = -1) {
     let mid = low + high - low / 2;
 
 
-    if (arr[mid] >= key) {
+    if (arr[mid] >key) {
         return lowerbound(arr, key, low, mid - 1, mid)
     } else {
         return lowerbound(arr, key, mid + 1, high, result)
@@ -19,7 +19,7 @@ function lowerbound(arr, key, low = 0, high = arr.length - 1, result = -1) {
 let result = lowerbound([1, 2, 3, 3, 3, 4, 5, 6, 7], 3);
 let result2 = lowerbound([1, 2, 3, 3, 3, 4, 5, 7], 6); 
 let result3 = lowerbound([1, 2, 3, 3, 3, 4, 5, 7], -1); 
-console.log(result, result2); 
+console.log(result, result2 ,result3); 
 
 //time - O(logn)
 //space -O(1)
