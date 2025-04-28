44b789668fe636c489a27c931c23258323610563
Kishukumar07 <kishukumars082@gmail.com>
2025-04-28 19:36:53 +0530

    did minimum in sorted and rotated array

--- 11.SearchingAlgos/level2/01.minimum in Sorted and Rotated Array.js
+++ 11.SearchingAlgos/level2/01.minimum in Sorted and Rotated Array.js
@@ -1,11 +1,22 @@
 // given sorted and rotated
 //Goal finding min in sorted and rotated array. 
-arr=[8,9,10,1,2,3,4,5,6]
- //we know min el always present in sorted part. 
-function minInSandR(arr,low,high ,resultIndex=-1){
-if(low>high) return resultIndex;  
+arr = [8, 9, 10, 1,2, 3, 4, 5, 6]
+// arr=[5,6,7,8,2,3]
+//we know min el always present in sorted part. 
+function minInSandR(arr, low, high) {
+    if (low >= high) return low;
 
-let mid = Math.floor(low+(high-low) /2); 
+    let mid = Math.floor(low + (high - low) / 2);
 
+    if (arr[mid] > arr[high]) {
+        //Right part is sorted one ...
+       return minInSandR(arr, mid + 1, high);
+    }
+    else {
+       return minInSandR(arr, low, mid); //left part is sorted  
+    }
 
 }
+
+let result = minInSandR(arr,0,arr.length-1)
+console.log(result)
