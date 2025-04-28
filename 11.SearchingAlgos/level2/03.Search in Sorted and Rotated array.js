9049240eff0915841124458ac158c9687355df21
Kishukumar07 <kishukumars082@gmail.com>
2025-04-28 20:09:15 +0530

    search key in S&R

---
+++ 11.SearchingAlgos/level2/03.Search in Sorted and Rotated array.js
@@ -0,0 +1,17 @@
+//goal: To find the index of given key in sorted and rotated array
+ 
+//Technique : =>   we 'll find the mid and we know both left and right part is sorted in itself 
+// and we also know finding the key index in sorted array ...
+// that's all needed to solve this question ....
+//we'll divide the main array in two part and will try to find the key's index for both part recursively 
+//oky we got the technique... 
+
+
+
+
+
+
+
+
+
+
