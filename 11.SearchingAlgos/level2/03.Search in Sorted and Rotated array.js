88daf5fe35791c07c77baf9486f9f5a6a0b1d9cb
Kishukumar07 <kishukumars082@gmail.com>
2025-04-29 15:02:09 +0530

    soling search in S&&R

--- 11.SearchingAlgos/level2/03.Search in Sorted and Rotated array.js
+++ 11.SearchingAlgos/level2/03.Search in Sorted and Rotated array.js
@@ -1,24 +1,43 @@
 //goal: To find the index of given key in sorted and rotated array
 
-//Technique : =>   we 'll find the mid and we know both left and right part is sorted in itself 
-// and we also know finding the key index in sorted array ...
-// that's all needed to solve this question ....
-//we'll divide the main array in two part and will try to find the key's index for both part recursively 
-//oky we got the technique... 
-
 let arr = [5, 6, 7, 8, 9, 1, 2, 3, 4];
 
-function keyIndexInSandR(arr, key, low = 0, high = arr.length - 1) {
-    
+
+
+
+function minel(arr, low = 0, high = arr.length - 1) {
+    if (low >= high) return high;
+
+    let mid = Math.floor(low + (high - low) / 2);
+
+    if (arr[mid] > arr[high]) {
+        return minel(arr, mid + 1, high);
+    }
+    else {
+        return minel(arr, low, mid);
+    }
 
 }
 
-console.log(keyIndexInSandR(arr, 1))
+
+// console.log(minElIndex); 
+
+
+
+function keyIndexInSandR(arr, key, minElIndex) {
 
 
 
 
 
+}
+
+
+
+
+
+let minElIndex = minel(arr);
+console.log(keyIndexInSandR(arr, 1, minElIndex))
 
 
 
