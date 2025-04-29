4370d75caf4ae307e13247564160dd964968ff00
Kishukumar07 <kishukumars082@gmail.com>
2025-04-29 20:42:10 +0530

    array is SandR

---
+++ 11.SearchingAlgos/level2/04.Check if array is sorted and Rotated or Not.js
@@ -0,0 +1,8 @@
+//Description: Check if weather a given array is sorted and rotated or not ?
+//App1 :- We'll find the min el index ~BS.Algo ... and then check the both L &R part if sorted or not... T.C->O(N)
+
+
+
+
+let arr=[5,6,7,8,9,1,2,3,4]; 
+//finding the min el index in S&R array...
