cee4c7ec8a8c61b89990337f544beee18cfe2cf5
Kishukumar07 <kishukumars082@gmail.com>
2025-04-30 09:16:56 +0530

    oky did

--- 11.SearchingAlgos/level2/06.finding the peak.js
+++ 11.SearchingAlgos/level2/06.finding the peak.js
@@ -1,8 +1,7 @@
-// Description: find the peak in Sorted and Rotated array.
+// Description: find the peak in Sorted and Rotated array. 
+//similar to :-  you remember the mimimun is S&R wala part ie Que ... we first trying to find the sorted wala part and there will be that min el present.
+let arr = [7, 11, 9, 4, 3, 1, 10, 2, 4]
 
 
-
-
-
-
+// the logic  "leftNeighbours < peakItem  > RightNeighbour" with binary search 
 
