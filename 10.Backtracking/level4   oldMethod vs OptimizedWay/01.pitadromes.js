5d64371391c4a05f86c156e37b711f659b1191cf
Kishukumar07 <kishukumars082@gmail.com>
2025-05-09 18:24:06 +0530

    pitadromes solved

---
+++ 10.Backtracking/level4   oldMethod vs OptimizedWay/01.pitadromes.js
@@ -0,0 +1,48 @@
+
+function runProgram(input) {
+
+    //   sub.lengh k and sum criteria
+
+    let k = +input;
+
+    let result = [];
+    gen(k, [], result);
+
+    console.log(result.join(" "));
+
+
+}
+
+
+function gen(k, sub, result) {
+
+    if (sub.length == k) {
+        let sumeven = 0, sumodd = 0;
+
+        for (let i = 0; i < sub.length; i++) {
+            if (i % 2 === 0) {
+                sumeven += sub[i];
+            }
+            else {
+                sumodd += sub[i];
+            }
+        }
+
+
+        if (sumeven === sumodd) {
+            result.push([...sub].join(""));
+        }
+        return;
+    }
+
+
+    let start = (sub.length === 0) ? 1 : 0;  // <-- if sub is empty, first digit from 1 to 9; otherwise 0 to 9
+    for (let i = start; i <= 9; i++) {
+        gen(k, [...sub, i], result);
+    }
+
+
+}
+
+
+
