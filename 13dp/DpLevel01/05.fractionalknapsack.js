a03581303508f6ea1639738ec1dfdd63f1a43c54
kishukumar07 <kishukumars082@gmail.com>
2025-06-18 20:38:46 +0530

    fractional knapsack

---
+++ 13dp/DpLevel01/05.fractionalknapsack.js
@@ -0,0 +1,40 @@
+//NORMAL GREEDY APPROACH  ...
+class Solution {
+  fractionalknapsack(val, wt, capacity) {
+    // code here
+    let arr = new Array(val.length);
+
+    for (let i = 0; i < val.length; i++) {
+      arr[i] = new Array(3).fill(0);
+    }
+
+    for (let i = 0; i < val.length; i++) {
+      arr[i][0] = val[i] / wt[i];
+      arr[i][1] = val[i];
+      arr[i][2] = wt[i];
+    }
+    arr.sort((a, b) => b[0] - a[0]);
+    return fun(arr, capacity);
+  }
+}
+
+function fun(arr, capacity) {
+  let maxVal = 0,
+    i = 0;
+
+  while (i < arr.length && capacity >= arr[i][2]) {
+    maxVal += arr[i][1];
+    capacity -= arr[i][2];
+    i++;
+  }
+
+  if (i < arr.length) {
+    maxVal += capacity * arr[i][0];
+  }
+
+  return maxVal;
+}
+
+
+
+
