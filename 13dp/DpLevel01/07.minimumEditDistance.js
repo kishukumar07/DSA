7a790acbf7337f81337f16f069b46a3167d52117
kishukumar07 <kishukumars082@gmail.com>
2025-06-19 12:02:28 +0530

    added bottom up approach for edit dist.

--- 13dp/DpLevel01/07.minimumEditDistance.js
+++ 13dp/DpLevel01/07.minimumEditDistance.js
@@ -28,3 +28,26 @@ function solve(s, t, n, m, dp) {
   let op3 = 1 + solve(s, t, n - 1, m - 1, dp); //del
   return (dp[n][m] = Math.min(op1, op2, op3));
 }
+
+// BOTTOM UP APPROACH...
+
+function solve(s, t, n, m, dp) {
+  for (let i = 0; i <= n; i++) {
+    dp[i][0] = i;
+  }
+  for (let i = 0; i <= m; i++) {
+    dp[0][i] = i;
+  }
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= m; j++) {
+      if (s[i - 1] == t[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
+      }
+    }
+  }
+
+  return dp[n][m];
+}
